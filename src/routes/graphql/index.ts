96f0b8b6812719d10c298bc024ef26dea38e163b
a-reznikov <a.reznnikov@gmail.com>
2024-11-04 22:50:49 +0300

    feat: implement depth limit validation

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,10 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { GraphQLSchema, graphql } from 'graphql';
+import { GraphQLSchema, graphql, parse, validate } from 'graphql';
 import { RootQueryType } from './queries.js';
 import { Mutations } from './mutations.js';
+import depthLimit from 'graphql-depth-limit';
+import { isErrorObject } from './utils/typeguards.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -17,6 +19,20 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      try {
+        const graphQlErrors = validate(schema, parse(req.body.query), [depthLimit(5)]);
+
+        if (graphQlErrors.length) {
+          return { errors: graphQlErrors };
+        }
+      } catch (error) {
+        if (isErrorObject(error)) {
+          throw fastify.httpErrors.badRequest(error.message);
+        }
+
+        throw new Error('Unknown error');
+      }
+
       return graphql({
         schema,
         source: req.body.query,
--- src/routes/graphql/types/common.ts
+++ src/routes/graphql/types/common.ts
@@ -10,3 +10,7 @@ export const RequiredFloat = new GraphQLNonNull(GraphQLFloat);
 export const RequiredInt = new GraphQLNonNull(GraphQLInt);
 export const RequiredBoolean = new GraphQLNonNull(GraphQLBoolean);
 export const RequiredString = new GraphQLNonNull(GraphQLString);
+
+export type ErrorObject = {
+  message: string;
+};
